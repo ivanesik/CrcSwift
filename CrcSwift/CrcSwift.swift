4947e4da05c2cbb9557bc416675bafcfcc5e7503
ivanesik <elyoskin@gmail.com>
2018-07-27 09:08:17 +0300

    Init commit

---
+++ CrcSwift/CrcSwift.swift
@@ -0,0 +1,11 @@
+//
+//  CrcSwift.swift
+//  CrcSwift
+//
+//  Created by Admin on 16.07.2018.
+//  Copyright © 2018 Ivan Elioskin. All rights reserved.
+//
+
+class CrcSwift {
+
+}
---
+++ CrcSwiftTests/CrcSwiftTests.swift
@@ -0,0 +1,28 @@
+//
+//  CrcSwiftTests.swift
+//  CrcSwiftTests
+//
+//  Created by Admin on 16.07.2018.
+//  Copyright © 2018 Ivan Elioskin. All rights reserved.
+//
+
+import XCTest
+
+class CrcSwiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    
+    func testExample() {
+    }
+
+    
+}
